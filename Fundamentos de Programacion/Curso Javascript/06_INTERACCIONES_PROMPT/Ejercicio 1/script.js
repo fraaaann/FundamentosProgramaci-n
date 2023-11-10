718ea6d494890f058dc64ef3aa2aba2781a52b0e
programacioniccp <149515058+programacioniccp@users.noreply.github.com>
2023-11-10 14:42:20 +0000

    ok

--- Fundamentos de Programacion/Curso Javascript/06_INTERACCIONES_PROMPT/Ejercicio 1/script.js
+++ Fundamentos de Programacion/Curso Javascript/06_INTERACCIONES_PROMPT/Ejercicio 1/script.js
@@ -1,13 +1,14 @@
 // Verificar si es mayor de Edad
 
+var numero = 20;
+
 function mayorEdad(){
     let edad = prompt("Por favor ingrese su Edad: ");
 
+    
     if(edad >= 18){
         alert("Eres mayor de edad")
     } else {
         alert("Es menor de edad")
     }
 }
-
-
